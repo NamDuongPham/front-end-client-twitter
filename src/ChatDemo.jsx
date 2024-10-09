3046627c703603348b063d9b80848a6297a4eff3
Nam Duong <phamnamduong583@gmail.com>
2024-10-09 17:53:16 +0700

    private message 2 people

--- src/ChatDemo.jsx
+++ src/ChatDemo.jsx
@@ -1,26 +1,54 @@
-import { useEffect } from "react";
-import { io } from "socket.io-client";
+import { useEffect, useState } from "react";
+import socket from "./socket";
 
 export default function ChatDemo() {
+  const profile = JSON.parse(localStorage.getItem("profile"));
+  const [value, setValue] = useState("");
+  const [conversations, setConversations] = useState([]);
+  // const [receiver, setReceiver] = useState("");
   useEffect(() => {
-    const socket = io(import.meta.env.VITE_API_URL); //VITE_API_URL="http://localhost:4000"
-    socket.on("connect", () => {
-      console.log(socket.id);
-      socket.emit("hello server",{
-        message: `Hi server đã kết nối thành công`
-      });
-      socket.on("hi", (data) => {
-        console.log(data);
-      });
+    socket.auth = {
+      _id: profile._id,
+    };
+    socket.connect();
+    socket.on("receive private message", (data) => {
+      // console.log(data);
+      // const { payload } = data;
+      const content = data.content;
+      setConversations((conversations) => [...conversations, content]);
     });
-
-    socket.on("disconnect", () => {
-      console.log(socket.id);
-    });
-
     return () => {
       socket.disconnect();
     };
-  }, []);
-  return <div>Chat</div>;
+  }, [profile._id]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setValue("");
+    socket.emit("private message", {
+      content: value,
+      to: "66f8e1577a3f80640fdfe2c0", //user_id
+    });
+  };
+  return (
+    <div>
+      <h1>Chat</h1>
+      <div>
+        {conversations.map((message, index) => (
+          <>
+            <div key={index}>
+              <div>{message}</div>
+            </div>
+          </>
+        ))}
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={value}
+          type="text"
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
 }
